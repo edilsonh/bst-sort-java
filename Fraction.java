50a61ca58b8fe946d5cd96786947e85adcd721bb
Edilson Hernandez <edilson.hernandez1@gmail.com>
2018-04-24 20:10:23 -0400

    Create Fraction class

---
+++ Fraction.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Fraction implements Comparable {
+  private ArrayList<T> f = new ArrayList<>();
+
+  public Fraction() {
+
+  }
+
+  public void insert() {
+
+  }
+
+  public void compareTo() {
+
+  }
+
+  public String toString() {
+
+  }
+}
